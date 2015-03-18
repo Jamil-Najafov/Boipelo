0d3dc54c02fce3ed66f1ab06b569df17124e4550
Aycan <Aycan@Corsair>
2015-03-18 22:34:12 +0200

    user can't edit other user's profiles

--- src/main/java/boipelo/repositories/UserRepository.java
+++ src/main/java/boipelo/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package boipelo.repositories;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import boipelo.domain.user.User;
 
@@ -11,5 +12,9 @@ public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 
 	// the "0" parameter is a workaround for a bug in SDN
 	User findUserByLogin(@Param("0") String login);
+
+	@PreAuthorize("#user.getId() == principal.id")
+	@Override
+	User save(@Param("user") User user);
 
 }
--- src/main/java/boipelo/spring/config/security/SecurityConfig.java
+++ src/main/java/boipelo/spring/config/security/SecurityConfig.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+@EnableGlobalMethodSecurity(prePostEnabled=true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
