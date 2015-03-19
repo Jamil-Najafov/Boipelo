c0e5c5792b02144bd545b77717ff8aa118f6a00d
Aycan <Aycan@Corsair>
2015-03-19 16:56:30 +0200

    user update authorization

---
+++ src/main/java/boipelo/domain/user/UserEventHandler.java
@@ -0,0 +1,17 @@
+package boipelo.domain.user;
+
+import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
+import org.springframework.data.rest.core.annotation.HandleBeforeSave;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+@RepositoryEventHandler(User.class)
+public class UserEventHandler {
+
+    @HandleBeforeSave
+    @PreAuthorize("#user.getId() == principal.id")
+    public void checkUpdateAuthority(User user) {
+        //only authority check
+    }
+
+}
